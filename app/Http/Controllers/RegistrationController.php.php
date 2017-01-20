cc5050abfa2f6fd22fb1e2388584f5411fb2e55b
Albertto Thamrin <albertto@thamr.in>
2017-01-20 19:33:04 +0700

    add migration & registration file

---
+++ app/Http/Controllers/RegistrationController.php.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController.php extends Controller
+{
+    //
+    public function registerform() 
+    {
+    	return view('registerform');
+    }    
+}
